b3aec0b6d50587414df03d648f6d3709fdee1a94
Tomohiro Suzuki <13636234+stomo0511@users.noreply.github.com>
2023-01-16 08:54:49 +0900

    print

--- detach-deps/ch_ompss.c
+++ detach-deps/ch_ompss.c
@@ -68,13 +68,15 @@ void cholesky_mpi(const int ts, const int nt, double *A[nt][nt], double *B, doub
 						omp_potrf(A[k][k], ts, ts);
 						END_TIMING(TIME_POTRF);
 					}
+					///////////////////////////////////////////////////////////////////////////
+					printf("POTRF done at %d\n" ,mype);
 				}
 
 				//////////////////////////////////////////////////////////
 				// Send task (POTRF)
 				if (block_rank[k*nt+k] == mype && np != 1)
 				{
-				   omp_event_handle_t event_handle;
+					omp_event_handle_t event_handle;
 					#pragma omp task depend(in: A[k][k]) firstprivate(k) depend(in: comm_round_sentinel) detach(event_handle)
 					{
 						START_TIMING(TIME_COMM);
@@ -103,6 +105,9 @@ void cholesky_mpi(const int ts, const int nt, double *A[nt][nt], double *B, doub
 						MPIX_Detach_all(nreqs, reqs, Detach_all_callback, (void*)event_handle);
 
 						END_TIMING(TIME_COMM);
+
+						///////////////////////////////////////////////////////////////////////////
+						printf("Send (POTRF) done from %d\n" ,mype);
 					}
 				}
 
@@ -128,16 +133,20 @@ void cholesky_mpi(const int ts, const int nt, double *A[nt][nt], double *B, doub
 						}
 						recv_flag = 0;
 					}
+					///////////////////////////////////////////////////////////////////////////
+					printf("Receive (POTRF) done at %d\n" ,mype);
 				}
 
 				#ifdef HAVE_INTERMEDIATE_COMM_SENTINEL
-						// sentinel task to limit communication task parallelism
+				// sentinel task to limit communication task parallelism
 				#pragma omp task depend(out: comm_round_sentinel)
-						{ if (comm_round_sentinel < 0) comm_round_sentinel = 0; }
+				{ if (comm_round_sentinel < 0) comm_round_sentinel = 0; }
 				#endif
 
 				for (int i = k + 1; i < nt; i++)
 				{
+					//////////////////////////////////////////////////////////
+					// TRSM
 					if (block_rank[k*nt+i] == mype)
 					{
 						num_comp_tasks++;
@@ -159,8 +168,12 @@ void cholesky_mpi(const int ts, const int nt, double *A[nt][nt], double *B, doub
 								END_TIMING(TIME_TRSM);
 							}
 						}
+						///////////////////////////////////////////////////////////////////////////
+						printf("TRSM done at %d\n" ,mype);
 					}
 
+					//////////////////////////////////////////////////////////
+					// Send task (TRSM)
 					if (block_rank[k*nt+i] == mype && np != 1)
 					{
 						for (int ii = k + 1; ii < i; ii++)
@@ -168,13 +181,16 @@ void cholesky_mpi(const int ts, const int nt, double *A[nt][nt], double *B, doub
 							if (!send_flags[block_rank[ii*nt+i]])
 								send_flags[block_rank[ii*nt+i]] = 1;
 						}
+
 						for (int ii = i + 1; ii < nt; ii++)
 						{
 							if (!send_flags[block_rank[i*nt+ii]])
 								send_flags[block_rank[i*nt+ii]] = 1;
 						}
+
 						if (!send_flags[block_rank[i*nt+i]])
 							send_flags[block_rank[i*nt+i]] = 1;
+
 						for (int dst = 0; dst < np; dst++)
 						{
 							if (send_flags[dst] && dst != mype)
@@ -193,8 +209,13 @@ void cholesky_mpi(const int ts, const int nt, double *A[nt][nt], double *B, doub
 							}
 						}
 						reset_send_flags(send_flags);
+
+						///////////////////////////////////////////////////////////////////////////
+						printf("Send (TRSM) done from %d\n" ,mype);
 					}
 
+					//////////////////////////////////////////////////////////
+					// Recive task (TRSM)
 					if (block_rank[k*nt+i] != mype)
 					{
 						for (int ii = k + 1; ii < i; ii++)
@@ -202,13 +223,16 @@ void cholesky_mpi(const int ts, const int nt, double *A[nt][nt], double *B, doub
 							if (block_rank[ii*nt+i] == mype)
 								recv_flag = 1;
 						}
+
 						for (int ii = i + 1; ii < nt; ii++)
 						{
 							if (block_rank[i*nt+ii] == mype)
 								recv_flag = 1;
 						}
+
 						if (block_rank[i*nt+i] == mype)
 							recv_flag = 1;
+
 						if (recv_flag)
 						{
 							recv_tasks++;
@@ -224,18 +248,24 @@ void cholesky_mpi(const int ts, const int nt, double *A[nt][nt], double *B, doub
 							}
 							recv_flag = 0;
 						}
+
+						///////////////////////////////////////////////////////////////////////////
+						printf("Receive (TRSM) done at %d\n" ,mype);
 					}
 				}
 
-				if ((max_send_tasks + max_recv_tasks) < (send_tasks + recv_tasks)) {
-				max_send_tasks = send_tasks;
-				max_recv_tasks = recv_tasks;
+				if ((max_send_tasks + max_recv_tasks) < (send_tasks + recv_tasks))
+				{
+					max_send_tasks = send_tasks;
+					max_recv_tasks = recv_tasks;
 				}
 
 				for (int i = k + 1; i < nt; i++)
 				{
 					for (int j = k + 1; j < i; j++)
 					{
+						//////////////////////////////////////////////////////////
+						// GEMM
 						if (block_rank[j*nt+i] == mype)
 						{
 							num_comp_tasks++;
@@ -274,9 +304,13 @@ void cholesky_mpi(const int ts, const int nt, double *A[nt][nt], double *B, doub
 									END_TIMING(TIME_GEMM);
 								}
 							}
-						}
+						} // End of GEMM task
+						///////////////////////////////////////////////////////////////////////////
+						printf("GEMM done at %d\n" ,mype);
 					} // End of J-loop
 
+					//////////////////////////////////////////////////////////
+					// SYRK
 					if (block_rank[i*nt+i] == mype)
 					{
 						num_comp_tasks++;
@@ -298,16 +332,25 @@ void cholesky_mpi(const int ts, const int nt, double *A[nt][nt], double *B, doub
 								END_TIMING(TIME_SYRK);
 							}
 						}
-					}
+						///////////////////////////////////////////////////////////////////////////
+						printf("SYRK done at %d\n" ,mype);
+					} // End of SYRK task
 				} // End of I-loop
 			} // End of K-loop
 			END_TIMING(TIME_CREATE);
+
+			///////////////////////////////////////////////////////////////////////////
+			printf("End all\n");
 		}
 
 		#pragma omp taskwait
+		///////////////////////////////////////////////////////////////////////////
+		printf("End taskwait\n");
 
 		END_TIMING(TIME_TOTAL);
 		MPI_Barrier(MPI_COMM_WORLD);
+		///////////////////////////////////////////////////////////////////////////
+		printf("End MPI_Barrier\n");
 	 // pragma omp single
 	}// pragma omp parallel
 
